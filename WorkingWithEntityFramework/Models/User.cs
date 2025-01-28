17f985b179079195a214dd7b3313cdeb5e4adadb
Mehdi Heidari <hymehdi84@gmail.com>
2025-01-28 19:24:43 +0300

    creating User Entity

---
+++ WorkingWithEntityFramework/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkingWithEntityFramework.Models;
+
+public class User
+{
+    public int  UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+    public string AboutMe { get; set; }
+    public DateTime CreateDate { get; set; }
+}
