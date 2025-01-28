334296ecf973af2f16bf0b48ed758258201e63ad
Mehdi Heidari <hymehdi84@gmail.com>
2025-01-28 19:27:44 +0300

    creatign db context class

---
+++ WorkingWithEntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkingWithEntityFramework.Models;
+
+namespace WorkingWithEntityFramework;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+    {
+        
+    }
+    public DbSet<User> Users { get; set; }
+}
