a4991e7e206f8d94e228aa25a74114602c4218cd
Mehdi Heidari <hymehdi84@gmail.com>
2025-01-28 19:31:59 +0300

    injection dependecy

--- WorkingWithEntityFramework/Program.cs
+++ WorkingWithEntityFramework/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WorkingWithEntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlogDatabase")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
